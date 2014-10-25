e516aa11979398fa67394aa66729a5b31678025b
pyrohail <justin@pyrohail.com>
2014-10-25 11:11:58 -0400

    Add documentation to VideoReader.java

--- src/main/java/com/pyrohail/averageframe/VideoReader.java
+++ src/main/java/com/pyrohail/averageframe/VideoReader.java
@@ -3,18 +3,37 @@ package com.pyrohail.averageframe;
 import org.opencv.core.Mat;
 import org.opencv.videoio.VideoCapture;
 
+
+/**
+ * Handles the input of video files
+ *
+ * @author Justin Harrison
+ */
 public final class VideoReader {
 	private final VideoCapture videoCapture;
 
+	/**
+	 * VideoReader constructor
+	 */
 	public VideoReader() {
 		videoCapture = new VideoCapture();
 	}
 
+	/**
+	 * VideoReader constructor
+	 *
+	 * @param fileName the location of a video file
+	 */
 	public VideoReader(String fileName) {
 		videoCapture = new VideoCapture();
 		open(fileName);
 	}
 
+	/**
+	 * VideoCapture.open() wrapper
+	 *
+	 * @param fileName the location of a video file
+	 */
 	public void open(String fileName) {
 		videoCapture.open(fileName);
 		if (!videoCapture.isOpened()) {
@@ -23,12 +42,23 @@ public final class VideoReader {
 		}
 	}
 
+	/**
+	 * VideoCapture.read() wrapper.
+	 * Each access automatically increases the current frame position.
+	 *
+	 * @return the current frame in the form of a Mat object
+	 */
 	public Mat getFrame() {
 		Mat frame = new Mat();
 		videoCapture.read(frame);
 		return frame;
 	}
 
+	/**
+	 * videoCapture getter
+	 *
+	 * @return videoCapture
+	 */
 	public VideoCapture getVideoCapture() {
 		return videoCapture;
 	}
